43bd4c944034933cfb33b3e73b398ef91ce1fe14
JXJ-GH <1315816541@qq.com>
2019-08-21 08:33:14 +0800

    实现商品全部列表展示。

---
+++ src/main/java/com/mall/food/controller/ListController.java
@@ -0,0 +1,34 @@
+package com.mall.food.controller;
+
+import com.mall.food.pojo.Business;
+import com.mall.food.pojo.Commodity;
+import com.mall.food.pojo.CommodityType;
+import com.mall.food.service.impl.BusinessServiceImpl;
+import com.mall.food.service.impl.CommodityServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class ListController {
+    @Autowired
+    CommodityServiceImpl commodityService;
+    @Autowired
+    BusinessServiceImpl businessService;
+    @RequestMapping("/tolist")
+    public String initListPage(Model model){
+        List<Commodity> commodityList= commodityService.selectAllCommodity();
+        model.addAttribute("allCommodity",commodityList);
+        List<CommodityType> commodityTypeList = commodityService.selectAllCommodityType();
+        model.addAttribute("cTypes",commodityTypeList);
+        System.out.println(commodityList);
+        return "list";
+    }
+    @RequestMapping("/toShopList")
+    public String initShopList(Model model){
+        return "";
+    }
+}
